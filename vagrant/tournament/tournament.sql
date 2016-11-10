b1f903d05ec55c32412b8c086f42fa9bc7f9151d
tloi <tloi@hotmail.com>
2016-11-10 00:52:29 -0500

    test

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,6 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+create table players (id SERIAL primary key, name TEXT);
 
+create table matches (id SERIAL primary key, winner INTEGER REFERENCES players (id), loser INTEGER REFERENCES players(id));
